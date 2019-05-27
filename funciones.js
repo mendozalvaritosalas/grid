d60aef9a9db39f38ff91db6978fc9805532d948c
Alvaro Ponci Mendoza Salas <mendozalvaritosalas@gmail.com>
2019-05-27 11:05:54 -0400

    add index.html, estilos.css and funciones.js completed

--- funciones.js
+++ funciones.js
@@ -1,15 +1,35 @@
-const bllenar = document.getElementById("llenar"); 
-let div = document.getElementById("main-content");
+const bllenar = document.getElementById("llenar");
+let divMain = document.getElementById("main-content");
+const divHijo = document.createElement("div");
+const divCreados = document.getElementsByClassName("fila");
 
-function crearDivisiones(n){
-    let fila;
+function crearDivisiones(n) {
+    divMain.innerHTML = "";
     for (let i = 0; i < n; i++) {
-        fila = div.appendChild("div");
-        div.setAttributeNS("class", "fila")
+        divHijo.className = "columna";
+        let x = divMain.appendChild(divHijo.cloneNode());
+        divHijo.className = "fila";
         for (let j = 0; j < n; j++) {
-            div.appendChild("div");
+            x.appendChild(divHijo.cloneNode());
         }
+    }
+    añadirEventos();
+}
+
+function colorAleatorio(element) {
+    var r = Math.round(Math.random() * 255);
+    var g = Math.round(Math.random() * 255);
+    var b = Math.round(Math.random() * 255);
+    var a = 1; //transparencia entre 0 a 1
+    let rgba = "rgba(" + r + ", " + g + ", " + b + ", " + a + ")";
+    element.style.backgroundColor = rgba;
+}
+
+function añadirEventos() {
+    for (let a = 0; a < divCreados.length; a++) {
+        divCreados[a].onmouseover = function () {colorAleatorio(divCreados[a])};
     }
 }
 
-bllenar.onclick = function(){crearDivisiones(prompt("ingrese una cantidad:"))};
+bllenar.onclick = function () { crearDivisiones(prompt("ingrese una cantidad:")) };
+
